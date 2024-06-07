ed931f56d297e213f0a65938e3a9cacbe97d0ffe
Royikiss <royi990001@gmail.com>
2024-06-07 13:28:30 +0800

    test

---
+++ deque.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+        > File Name: deque.cpp
+      > Author:Royi
+      > Mail:royi990001@gmail.com
+      > Created Time: Fri 07 Jun 2024 01:25:31 PM HKT
+      > Describe:
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <ctype.h>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <functional>
+
+
+int main() {
+
+  return 0;
+}
