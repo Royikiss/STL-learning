597a03cb50c64b5b058855568b3942eace3444f4
Royikiss <royi990001@gmail.com>
2024-06-10 12:44:38 +0800

    update for vector

--- src/vector.cpp
+++ src/vector.cpp
@@ -34,15 +34,67 @@ public :
         _Finish = data + __size;
         _M_pos = data;
     }
-    vector(const vector )
+    vector(const vector &v) {
+        __size = v.__size;
+        data = (T *)malloc(sizeof(T) * __size);
+        for (size_t i = 0; i < v.size(); ++i) {
+            new(data + i) T(v[i]); 
+        }
+        _Finish = data + __size;
+        _M_pos = data + v.size();
+    }
+    vector(vector &&v) {
+        __size  = v.__size;
+        data    = v.data;
+        _M_pos  = v._M_pos;
+        _Finish = v._Finish;
+        v.data  = v._M_pos = v._Finish = v._M_pos = nullptr;
+    }
+    
+    ~vector() {
+        if (data == nullptr) return ; 
+        for (size_t i = 0; i < __size; ++i) {
+            data[i].~T();
+        }
+        free(data);
+        return ;
+    }
 
-
-
+    iterator begin() const { return data; } 
+    iterator end() const { return _M_pos; } 
+    
+    T &operator[](size_t ind) const {
+        return data[ind];
+    }
+    
+    size_t size() { return _M_pos - data; }
+    
+    void push_back(const T &obj) {
+        if (_M_pos == _Finish && !__expand()) {
+            std::cout << "expand faild!" << std::endl;
+            return ;
+        }
+        new(_M_pos) T(obj);
+        _M_pos += 1;
+        return ;
+    }
 
 private :
     size_t __size;
     T *data;
     T *_M_pos, *_Finish;
+    
+    bool __expand() {
+        T *p = (T *)realloc(data, sizeof(T) * __size * 2);
+        if (p == nullptr) { return false; }
+        size_t offset = _M_pos - data;
+        __size *= 2;
+        data = p;
+        _M_pos = data + offset;
+        _Finish = data + __size;
+        return true;
+    }
+    
 };
 }
 
@@ -59,8 +111,6 @@ int main() {
     for (auto x : v1) {
         std::cout << x << ' ';
     }
-    std::cout << std::endl;
-    
 
     return 0;
 }
